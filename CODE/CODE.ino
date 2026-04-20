a83b867ec8451486216534aaf5763e1c6d744f76
Nicolas Jao <nicojao@icloud.com>
2026-04-20 03:47:48 -0400

    fixed matrix pin mappings on layout. updated bitmap silkscreens.

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -1,60 +1,38 @@
 #include <LedControl.h>
 #include <avr/wdt.h>
 
-#define BUTTON_PIN 4
+#define MIC_PIN A0
+#define THRESHOLD 100000  // adjust this up or down based on your mic sensitivity 513
 
 LedControl lc = LedControl(/*DIN=*/11, /*CLK=*/13, /*CS=*/10, /*numDevices=*/1);
 
-bool showHeart = false;
-bool lastButtonState = HIGH;
-
-byte pattern[8] = {
-  B00000000,
-  B11100111,
-  B00100100,
-  B11100111,
-  B00000000,
-  B00000000,
-  B00100100,
-  B00111100
-};
-
-byte heart[8] = {
-  B00000000,
-  B01100110,
-  B11111111,
-  B11111111,
-  B01111110,
-  B00111100,
-  B00011000,
-  B00000000
-};
-
-void displayPattern(byte p[]) {
+void setAll(bool on) {
+  byte val = on ? B11111111 : B00000000;
   for (int row = 0; row < 8; row++) {
-    lc.setRow(0, row, p[row]);
+    lc.setRow(0, row, val);
   }
 }
 
 void setup() {
   wdt_disable();
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
   delay(500);
   lc.shutdown(0, false);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
-  displayPattern(pattern);
 }
 
 void loop() {
-  bool currentButtonState = digitalRead(BUTTON_PIN);
+  int sample = analogRead(MIC_PIN);
+  int centered = abs(sample - 512);
 
-  if (lastButtonState == HIGH && currentButtonState == LOW) {
-    showHeart = !showHeart;
-    lc.clearDisplay(0);
-    displayPattern(showHeart ? heart : pattern);
-    delay(50);  // debounce
+  if (centered > THRESHOLD) {
+    // sound detected - stop blinking, turn full board on
+    setAll(true);
+  } else {
+    // no sound - blink
+    setAll(true);
+    delay(100);
+    setAll(false);
+    delay(100);
   }
-
-  lastButtonState = currentButtonState;
 }
