70f5f753d203f51bf7c559d8c302499ed710d93c
Nicolas Jao <nicojao@icloud.com>
2026-04-20 00:52:18 -0400

    fixed wrong matrix pin mappings schematic.

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -1,9 +1,60 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LedControl.h>
+#include <avr/wdt.h>
 
+#define BUTTON_PIN 4
+
+LedControl lc = LedControl(/*DIN=*/11, /*CLK=*/13, /*CS=*/10, /*numDevices=*/1);
+
+bool showHeart = false;
+bool lastButtonState = HIGH;
+
+byte pattern[8] = {
+  B00000000,
+  B11100111,
+  B00100100,
+  B11100111,
+  B00000000,
+  B00000000,
+  B00100100,
+  B00111100
+};
+
+byte heart[8] = {
+  B00000000,
+  B01100110,
+  B11111111,
+  B11111111,
+  B01111110,
+  B00111100,
+  B00011000,
+  B00000000
+};
+
+void displayPattern(byte p[]) {
+  for (int row = 0; row < 8; row++) {
+    lc.setRow(0, row, p[row]);
+  }
+}
+
+void setup() {
+  wdt_disable();
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  delay(500);
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
+  lc.clearDisplay(0);
+  displayPattern(pattern);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool currentButtonState = digitalRead(BUTTON_PIN);
 
+  if (lastButtonState == HIGH && currentButtonState == LOW) {
+    showHeart = !showHeart;
+    lc.clearDisplay(0);
+    displayPattern(showHeart ? heart : pattern);
+    delay(50);  // debounce
+  }
+
+  lastButtonState = currentButtonState;
 }
